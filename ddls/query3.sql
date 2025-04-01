b1e99509ef06eb4e9535694eaceeb9d3b42e8742
aayyadevara02 <92749010+aayyadevara02@users.noreply.github.com>
2025-04-01 18:04:06 -0500

    Update query3.sql

--- ddls/query3.sql
+++ ddls/query3.sql
@@ -9,14 +9,17 @@ JOIN Category c ON q.category_id = c.category_id
 JOIN GameSession gs ON gsq.session_id = gs.session_id
 JOIN User u ON gs.user_id = u.user_id
 WHERE gsq.is_correct = TRUE
-GROUP BY c.name, u.username
+GROUP BY c.name, u.username, q.category_id
 HAVING COUNT(*) = (
-    SELECT MAX(correct_count) FROM (
-        SELECT COUNT(*) AS correct_count
+    SELECT MAX(correct_count)
+    FROM (
+        SELECT q2.category_id, COUNT(*) AS correct_count
         FROM GameSessionQuestion gsq2
         JOIN Question q2 ON gsq2.question_id = q2.question_id
-        WHERE gsq2.is_correct = TRUE AND q2.category_id = c.category_id
-        GROUP BY gsq2.session_id
+        WHERE gsq2.is_correct = TRUE
+        GROUP BY gsq2.session_id, q2.category_id
     ) AS sub
+    WHERE sub.category_id = q.category_id
 )
 LIMIT 15;
+
