a8cca6e2d03964a5d5eade0990e40f4b35751ef4
Shray Srivastava <shray3420@gmail.com>
2025-04-01 17:01:55 -0500

    changes

---
+++ ddls/insert_categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO Category (category_id, name, description) VALUES
+(1, 'Sports', 'Questions related to popular sports, players, and competitions.'),
+(2, 'Video Games', 'Trivia on classic and modern video games, characters, and gameplay.'),
+(3, 'Music', 'Covers artists, albums, instruments, and music history.'),
+(4, 'Movies', 'Includes questions about films, actors, directors, and movie trivia.');
