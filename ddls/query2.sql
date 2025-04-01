9f33295c1cd80c1edd028671a36b8d36ded2d4cb
aayyadevara02 <92749010+aayyadevara02@users.noreply.github.com>
2025-04-01 17:42:37 -0500

    Update query2.sql

--- ddls/query2.sql
+++ ddls/query2.sql
@@ -1,23 +1,16 @@
--- Leaderboard
-SELECT 
+SELECT
     u.username,
     total_scores.total_score,
     total_scores.total_sessions,
-    ROUND(total_scores.total_score / total_scores.total_sessions, 2) AS avg_score_per_session,
-    RANK() OVER (ORDER BY total_scores.total_score DESC) AS rank
-FROM 
-    (
-        SELECT 
-            g.user_id,
-            SUM(g.score) AS total_score,
-            COUNT(*) AS total_sessions
-        FROM 
-            GameSession g
-        GROUP BY 
-            g.user_id
-    ) AS total_scores
-JOIN 
-    User u ON total_scores.user_id = u.user_id
-ORDER BY 
-    total_scores.total_score DESC
+    ROUND(total_scores.total_score / total_scores.total_sessions, 2) AS avg_score_per_session
+FROM (
+    SELECT
+        g.user_id,
+        SUM(g.score) AS total_score,
+        COUNT(*) AS total_sessions
+    FROM GameSession g
+    GROUP BY g.user_id
+) AS total_scores
+JOIN User u ON total_scores.user_id = u.user_id
+ORDER BY total_scores.total_score DESC
 LIMIT 15;
